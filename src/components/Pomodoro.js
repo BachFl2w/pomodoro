90086a14491bbe05f0bf75eb2a2e589a75c92d23
Bach Nguyen <nguyen.van.bach@sun-asterisk.com>
2019-08-08 14:44:28 +0700

    Updated Yarn lockfile

---
+++ src/components/Pomodoro.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import './pomodoro.scss';
+
+const defaultValue = {
+  minutes: 24,
+  breakTime: 4,
+  seconds: 59,
+  type: 1, // 1: play time, 0: break time
+  play: false
+};
+
+export default class Pomodoro extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = defaultValue;
+
+    this.handleStart = this.handleStart.bind(this);
+    this.restartInterval = this.restartInterval.bind(this);
+    this.handleRestart = this.handleRestart.bind(this);
+    this.handleCountDown = this.handleCountDown.bind(this);
+  }
+
+  handleRestart() {
+    clearInterval(this.interval);
+    this.setState(defaultValue);
+  }
+
+  restartInterval() {
+    clearInterval(this.interval);
+    this.interval = setInterval(this.handleCountDown, 1000);
+  }
+
+  handleStart() {
+    const { minutes, breakTime, seconds, play } = this.state;
+
+    if (minutes < 0 || seconds < 0 || breakTime < 0) return;
+
+    if (play === true) {
+      this.handleRestart();
+    }
+
+    this.restartInterval();
+
+    this.setState({ play: true });
+  }
+
+  handleCountDown() {
+    const { minutes, seconds, type, breakTime } = this.state;
+    const newSeconds = seconds - 1 < 0 ? 59 : seconds - 1;
+    let newMinutes = type ? minutes : breakTime;
+
+    if (seconds - 1 < 0) {
+      newMinutes--;
+    }
+
+    if (newMinutes < 0) {
+      newMinutes = type ? defaultValue.breakTime - 1 : defaultValue.minutes;
+    }
+
+    if (newMinutes >= 0 && newSeconds >= 0) {
+      this.setState({
+        minutes: newMinutes,
+        seconds: newSeconds,
+        type: 1
+      });
+    } else if (type === 1) {
+      this.setState({
+        minutes: newMinutes,
+        type: 0
+      });
+    } else {
+      this.setState(defaultValue);
+    }
+  }
+
+  render() {
+    const { minutes, seconds } = this.state;
+
+    return (
+      <div className="container">
+        <div className="time" onClick={this.handleStart}>
+          <span className="minutes">{minutes}</span> : <span className="seconds">{seconds}</span>
+        </div>
+        <div className="footer">
+          <a href="https://www.facebook.com/bachnguyen29071998">Created by Flynn</a>
+        </div>
+      </div>
+    )
+  }
+}
